5382d040e073d7d15b836831d125c9d3daee6d07
xorua117 <xorua117@gmail.com>
2021-07-23 21:30:26 +0900

    Improving signup with unique email addresses

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -12,13 +12,13 @@ async function handler(req, res) {
 
   if (
     !email ||
-    !email.includes('@') ||
+    !email.includes("@") ||
     !password ||
     password.trim().length < 7
   ) {
     res.status(422).json({
       message:
-        'Invalid input - password should also be at least 7 characters long.',
+        "Invalid input - password should also be at least 7 characters long.",
     });
     return;
   }
@@ -26,6 +26,14 @@ async function handler(req, res) {
   const client = await connectToDatabase();
 
   const db = client.db();
+
+  const existingUser = await db.collection("users").findOne({ email: email });
+
+  if (existingUser) {
+    res.status(422).json({ message: "User exists already!" });
+    client.close();
+    return;
+  }
 
   const hashedPassword = await hashPassword(password);
 
@@ -35,6 +43,7 @@ async function handler(req, res) {
   });
 
   res.status(201).json({ message: "Created user!" });
+  client.close();
 }
 
 export default handler;
