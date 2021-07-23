3636fa2de410f518109fe021af96d4efeb9d5307
xorua117 <xorua117@gmail.com>
2021-07-23 21:56:49 +0900

    Sending a signin request from the frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,4 +1,6 @@
 import { useState, useRef } from "react";
+import { signIn } from "next-auth/client";
+
 import classes from "./auth-form.module.css";
 
 async function createUser(email, password) {
@@ -13,7 +15,7 @@ async function createUser(email, password) {
   const data = await response.json();
 
   if (!response.ok) {
-    throw new Error(data.message || 'Something went wrong!');
+    throw new Error(data.message || "Something went wrong!");
   }
 
   return data;
@@ -38,7 +40,15 @@ function AuthForm() {
     // optional: Add validation
 
     if (isLogin) {
-      // log user in
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+
+      if (!result.error) {
+        // set some auth state
+      }
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
